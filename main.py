d768b804b2c393e75dffdf10b8adfe3c000cda8a
pavlik <morgolpavel182@gmail.com>
2024-05-09 22:59:36 +0300

    commit

--- main.py
+++ main.py
@@ -1,26 +1,36 @@
 
-
 n = int(input())
 
 strings = [ input() for i in range(n)]
 
 nums = []
 anton = "anton"
-flag = False
 
+string2 = ''
 for string in strings:
+    flag = True
     string1 = string
     for c in string:
         if c not in anton:
             string1 = string1.replace(c, '')
         string2 = string1
-    for i in range(len(string1)):
-        for j in range(len(anton)):
-            if string1[j] != anton[j]:
-                string2 = string2[i + 1:]
-            continue
+    if len(string2) < len(anton):
+        continue
+    for i in range(len(anton)):
+        for j in range(len(string1)):
+            for g in range(len(anton)):
+                if string2[g] != anton[g]:
+                    string2 = string2[0:g] + string2[g + 1:]
+                    break
+                if string2[:5] == anton:
+                    flag = False
+                    string2 = string2[:5]
+                    break
+            if not flag:
+                break
+        if not flag:
+            break
+
     if string2 == anton:
-        nums.append(strings.index(string))
+        nums.append(strings.index(string) + 1)
 print(*nums)
-
-
